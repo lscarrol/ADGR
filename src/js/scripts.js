3ee53e61a27f130d0c263b979385980939fee05c
Liam S. Carroll <lcarroll@ADWLAW.com>
2018-08-02 16:54:45 -0400

    need to make scroll indicator better

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -44,3 +44,11 @@ setInterval( function() {
     $(dive1).removeClass('blurr');
   }
 }, 250 );
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
